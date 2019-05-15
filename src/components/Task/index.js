e8cf2ff21ac67679e01eef5b5d2d6849d1d634f2
Quang Huy <dqhuy78@gmail.com>
2019-05-15 21:57:27 +0700

    Update missing function

--- src/components/Task/index.js
+++ src/components/Task/index.js
@@ -30,13 +30,9 @@ const Task = (props) => (
                                 <div className="Task__editing-bgr" onClick={props.handleCancelEdit}></div>
                             </div>
                             : <Fragment>
-                                {
-                                    props.task.get('time') &&
-                                    <div className="Task__time">
-                                        <i className="far fa-calendar-alt"></i> {props.task.get('time')}
-                                    </div>
-                                }
-
+                                <div className="Task__time">
+                                    <i className="far fa-calendar-alt"></i> {props.task.get('time')}
+                                </div>
                                 <div className="Task__main">
                                     <div className="Task__content">
                                         {props.task.get('content')}
